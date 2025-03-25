092492b4c7e7558f1e2f956d11af072756b43a5b
Miraculin <colton.cheung@hotmail.ca>
2025-03-24 23:48:22 -0700

    update to use configuratoin class

--- test_main.py
+++ test_main.py
@@ -1,19 +1,18 @@
-import json
 import weather.weather as wm
 import ecalendar.ecalendar as ecal
 import ecalendar.event as event
 from datetime import date, timedelta
+from config import Config
 
 def main():
 
-    with open('config.json', 'r') as config_file:
-        config_data = json.load(config_file)
+    conf = Config.fromJson()
 
-    lat = config_data["latitude"]
-    lon = config_data["longitude"]
-    API_KEY = config_data["weather_api_key"]
+    weather_api_key = conf.weather_api_key
+    lat = conf.latitude
+    lon = conf.longitude
     
-    weather_manager = wm.Weather(lat, lon, API_KEY)
+    weather_manager = wm.Weather(lat, lon, weather_api_key)
     cal = ecal.ECalendar()
 
     #load events
@@ -22,6 +21,8 @@ def main():
     fake_event_2 = event.Event("Friend 2 Birthday", date.today(), date(year=2023,month=9,day=30))
     fake_events = [fake_event, fake_event_2]
     cal.add_events(fake_events)
+
+    cal.render(weather_manager.get_weather(), conf, output_path="test.bmp")
 
 if __name__ == "__main__":
     main()
